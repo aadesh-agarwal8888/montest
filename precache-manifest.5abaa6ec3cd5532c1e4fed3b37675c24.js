ce360b5d06aed61027270e56a36f604cf756c59f
aadesh.agarwal8888 <aadesh.agarwal8888@gmail.com>
2020-05-07 01:17:30 +0530

    Updates

---
+++ precache-manifest.5abaa6ec3cd5532c1e4fed3b37675c24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16627cfdb26f3582df4da9eedcc868ee",
+    "url": "/montest/index.html"
+  },
+  {
+    "revision": "a3ed1f3e8917e439be5d",
+    "url": "/montest/static/css/main.7490d772.chunk.css"
+  },
+  {
+    "revision": "ec8adf9eefc8a43f2637",
+    "url": "/montest/static/js/2.d1aa5fff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/montest/static/js/2.d1aa5fff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ed1f3e8917e439be5d",
+    "url": "/montest/static/js/main.915a182f.chunk.js"
+  },
+  {
+    "revision": "fce5aebcfeaf511fd023",
+    "url": "/montest/static/js/runtime-main.604ccb44.js"
+  }
+]);
